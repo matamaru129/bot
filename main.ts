28c3f729ec4826b720ee83216c4308e34b171012
Driver <163106292+matamaru129@users.noreply.github.com>
2025-03-09 14:23:52 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,8 @@ import "$std/dotenv/load.ts";
 
 // Botのトークンを.envから取得
 const BotToken: string = Deno.env.get("BOT_TOKEN")!;
-
 // プレフィックス「Bot 」を取り除く
 const cleanToken = BotToken.replace(/^Bot /, "");
-
 // 送信先チャンネルのIDを指定
 const channelId = "1343926776757354579"; // ここにチャンネルIDを設定
 
@@ -14,19 +12,18 @@ const channelId = "1343926776757354579"; // ここにチャンネルIDを設定
 const bot = createBot({
     token: cleanToken,  // プレフィックスを削除したトークンを使用
     botId: getBotIdFromToken(cleanToken) as bigint,
-
     // イベント発火時に実行する関数など
     events: {
         // 起動時
-        ready: async (_bot, payload) => {
+        ready: (_bot, payload) => {
             console.log(`${payload.user.username} is ready!`);
-                        // 10秒ごとに「ナイト」と送信する
+            
+            // 10秒ごとに「ナイト」と送信する
             setInterval(async () => {
                 await sendMessage(channelId, { content: "ナイト" });
                 console.log("Message sent: ナイト");
             }, 10000); // 10000ミリ秒 = 10秒
         },
-
         // メッセージ受信時
         messageCreate: async (_bot, message) => {
             // メッセージが「フォート」と一致する場合
@@ -40,7 +37,3 @@ const bot = createBot({
 
 // ボットを起動
 await startBot(bot);
-
-Deno.cron("Continuous Request", "*/2 * * * *", () => {
-    console.log("running...");
-});
